a84f28aa9628f42ead5218ac9763222c2381c856
Divyansh Chaudhary <divyansh7c@gmail.com>
2022-08-30 11:48:38 +0530

    Update help.py

--- PongV2/help.py
+++ PongV2/help.py
@@ -1,9 +1,94 @@
-from main import Powerups
 import pygame as pg
 pg.init()
 
 HELP_FONT = pg.font.SysFont("Freshman", 50)
 BUTTON_FONT = pg.font.SysFont("Freshman", 100)
+POWER_WIDTH, POWER_HEIGHT = (75, 75)
+COLOR = {
+    "BLACK": (0, 0, 0),
+    "WHITE": (255, 255, 255),
+    "GREEN": (8, 255, 8),
+    "GREEN2": (0, 255, 0),
+    "RED": (255, 0, 0),
+    "YELLOW": (255, 255, 0),
+    "PINK": (255, 105, 180),
+    "GOLD": (255, 215, 0),
+    "AQUA": (127, 255, 212)
+}
+SYMBOL_FONT = pg.font.SysFont("Freshman", 40)
+
+
+class Powerups:
+    # Class for powerups
+    def __init__(self, x, y, win):
+        self.x = x
+        self.y = y
+        self.win = win
+        self.symbol = ""
+        self.power_color = ""
+        self.symbol_color = ""
+        self.width = POWER_WIDTH
+        self.height = POWER_HEIGHT
+
+    # Expand -> + Board Length -> +
+    def expand(self):
+        self.symbol = "+"
+        self.power_color = COLOR["GREEN2"]
+        self.symbol_color = COLOR["PINK"]
+
+    # Shrink -> - Board Length -> -
+    def shrink(self):
+        self.symbol = "-"
+        self.power_color = COLOR["RED"]
+        self.symbol_color = COLOR["RED"]
+
+    # Slow Board -> Slow Opponent -> S
+    def slow_board(self):
+        self.symbol = "S"
+        self.power_color = COLOR["GREEN2"]
+        self.symbol_color = COLOR["PINK"]
+
+    # Slow Ball -> Ball to Minimum speed -> 0
+    def slow_ball(self):
+        self.symbol = "0"
+        self.power_color = COLOR["GREEN2"]
+        self.symbol_color = COLOR["PINK"]
+
+    # Extreme Ball -> Sudden Max Velocity -> Make ball Red -> F
+    def extreme_ball(self):
+        self.symbol = "F"
+        self.power_color = COLOR["RED"]
+        self.symbol_color = COLOR["RED"]
+
+    # Double Ball -> Double the number of balls -> Switch y_vel -> D
+    def double_ball(self):
+        self.symbol = "D"
+        self.power_color = COLOR["RED"]
+        self.symbol_color = COLOR["RED"]
+
+    # Golden Ability -> -1 Point -> G
+    def golden(self):
+        self.symbol = "G"
+        self.power_color = COLOR["GOLD"]
+        self.symbol_color = COLOR["GOLD"]
+
+    # Shield -> Ball cant pass -> P
+    def shield(self):
+        self.symbol = "P"
+        self.power_color = COLOR["GOLD"]
+        self.symbol_color = COLOR["GOLD"]
+
+    # Draw powerups
+    def draw_powerup(self):
+        outer_rect = pg.Rect(self.x, self.y, self.width, self.height)
+        inner_rect = pg.Rect(self.x + 5, self.y + 5, self.width - 10, self.height - 10)
+        inner_rect_x_middle = (self.x + 5 + inner_rect.width // 2)
+        inner_rect_y_middle = (self.y + 5 + inner_rect.height // 2)
+        symbol = SYMBOL_FONT.render(str(self.symbol), True, self.symbol_color)
+        pg.draw.rect(self.win, self.power_color, outer_rect)
+        pg.draw.rect(self.win, COLOR["WHITE"], inner_rect)
+        self.win.blit(symbol, (inner_rect_x_middle - symbol.get_width()//2,
+                               inner_rect_y_middle - symbol.get_height()//2))
 
 
 def draw_help(win, width, height, color, event=pg.event.Event(pg.KEYUP)):
@@ -46,7 +131,7 @@ def draw_help(win, width, height, color, event=pg.event.Event(pg.KEYUP)):
 
     expand_text = "Expand - Increase paddle length by few pixels"
     shrink_text = "Shrink - Reduce paddle length by few pixels"
-    slow_board_text = "Slow Board - Reduce opponent paddle movement speed"
+    slow_board_text = "Slow Board - Reduce opponent paddle movement speed (5 Sec)"
     slow_ball_text = "Slow Ball - Reduce ball speed to default"
     extreme_ball_text = "Extreme Ball - Increase ball speed above maximum"
     double_ball_text = "Double Ball - Doubles the number of ball on screen (Max: 4)"
